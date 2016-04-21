8f9ed7bb254fe334f2d572442e9d3efb23dcef45
afxefx <sullivanj@ecu.edu>
2016-04-21 13:34:22 -0400

    Initial commit
    Reinstall jamf from github

---
+++ device_sig_error_fix.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+qaURL=$1
+
+
+if /sbin/ping -oq $2 &> /dev/null
+
+    then
+
+    /bin/echo "grabbing package"
+    /usr/bin/curl --silent --output QuickAdd.pkg.zip "$qaURL"
+
+    /bin/echo "unzipping package"
+    sudo unzip QuickAdd.pkg.zip -d .;sudo rm -rf __MACOSX
+
+    /bin/echo "updating time"
+    sudo ntpdate -u $(systemsetup -getnetworktimeserver|awk '{print $4}')
+
+    /bin/echo "removing jamf framework"
+    sudo jamf removeFramework -verbose
+
+    /bin/echo "installing QuickAdd package"
+    sudo installer -dumplog -verbose -pkg QuickAdd.pkg -target /
+
+    /bin/echo "running a(nother) recon"
+    sudo /usr/local/bin/jamf recon -verbose
+
+## Thanks to Rich Trouton for the following gem!    
+
+    ## /bin/echo "installing CasperCheck"
+    ## sudo /usr/local/bin/jamf policy -trigger installCasperCheck
+
+
+    ## /bin/echo "flushing policy history"
+    ## sudo /usr/local/bin/jamf flushPolicyHistory
+
+    ## /bin/echo "running policy"
+    ## sudo /usr/local/bin/jamf policy -verbose
+
+else
+
+/bin/echo "Can't reach Casper.  Quitting."
+
+fi
+
+exit 0
