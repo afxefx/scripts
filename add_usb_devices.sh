d055d5fdbd7ac214b1e1cd18f249f8afe448e205
Jerry Sullivan <afxefx@users.noreply.github.com>
2018-10-11 15:36:33 -0400

    Disable connection of new devices

--- add_usb_devices.sh
+++ add_usb_devices.sh
@@ -14,16 +14,19 @@ if ! [ -e "${vmxcfg}" ]; then
         new0='usb.autoConnect.device0 = "vid:082b autoclean:0"'
         new1='usb.autoConnect.device1 = "vid:0b95 autoclean:0"'
         new2='usb.autoConnect.device2 = "vid:0fe2 autoclean:0"'
+        new3='usb.generic.autoconnect = "FALSE"'
                 
         echo ""
         echo "Updating USB auto connect values"
         sed -i '' -e '/'usb.autoConnect.device*'/d' /Library/Application\ Support/VMware/Aycan.vmwarevm/Aycan.vmx
+        sed -i '' -e '/'usb.generic.autoconnect*'/d' /Library/Application\ Support/VMware/Aycan.vmwarevm/Aycan.vmx
         #sed -i '' -e 's/'"$curr1"'/'"$new1"'/g' /Library/Application\ Support/VMware/Aycan.vmwarevm/Aycan.vmx
         #sed -i '' -e 's/'"$curr2"'/'"$new2"'/g' /Library/Application\ Support/VMware/Aycan.vmwarevm/Aycan.vmx
         #sed -i '' -e '/'usb.autoConnect.device3'/d' /Library/Application\ Support/VMware/Aycan.vmwarevm/Aycan.vmx
         echo $new0 >> /Library/Application\ Support/VMware/Aycan.vmwarevm/Aycan.vmx
         echo $new1 >> /Library/Application\ Support/VMware/Aycan.vmwarevm/Aycan.vmx
         echo $new2 >> /Library/Application\ Support/VMware/Aycan.vmwarevm/Aycan.vmx
+        echo $new3 >> /Library/Application\ Support/VMware/Aycan.vmwarevm/Aycan.vmx
         echo ""
 
         echo "Config file after alterations:"
